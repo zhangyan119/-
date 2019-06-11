d6104343ae680355a2ad0028af4f6d1674e5a402
zy <516222694@qq.com>
2019-06-11 16:29:49 +0800

    第二次提交

--- HelloGit/src/cn/itcast/FirstGit.java
+++ HelloGit/src/cn/itcast/FirstGit.java
@@ -3,5 +3,9 @@ package cn.itcast;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("锄禾日当午");
     }
+
+
 }
